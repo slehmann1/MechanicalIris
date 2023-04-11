bad57896f31639032b2764bc71f53cf88955ebe6
Samuel Lehmann <samueljlehmann@gmail.com>
2023-04-11 07:43:43 -0700

    Refactor blade calculations

--- blade.py
+++ blade.py
@@ -1,6 +1,6 @@
 import functools
 import math
-import time
+from dataclasses import dataclass
 
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
@@ -8,7 +8,24 @@ import numpy as np
 from scipy.optimize import fsolve
 
 
+@dataclass
+class Coordinate:
+    x: float
+    y: float
+
+
+@dataclass
+class BladeState:
+    o_4: Coordinate
+    a: Coordinate
+    b: Coordinate
+    g: Coordinate
+
+
 class blade:
+    NUM_POINTS = 100
+    PLOT_RANGE = [-100, 100, -100, 100]
+
     def __init__(self, a, b, c, d, e, f, g, theta_1, theta_5):
         self.a = a
         self.b = b
@@ -19,93 +36,158 @@ class blade:
         self.g = g
         self.theta_1 = theta_1
         self.theta_5 = theta_5
-        self.calc_points()
+        blade_states = self.calc_blade_states(200 * np.pi / 180, 280 * np.pi / 180)
+        self.plot_blade_motion(blade_states)
 
-    def calc_points(self):
-        theta_2s = np.arange(200, 280, 1)
-        thetas = [self.calc_thetas(theta_2 * np.pi / 180) for theta_2 in theta_2s]
-        o_4_points = [self.calc_point_o4() for _ in thetas]
-        b_points = [self.calc_point_b(theta[1], o_4_points[0]) for theta in thetas]
-        a_points = [
-            self.calc_point_a(theta_2 * np.pi / 180, (0, 0)) for theta_2 in theta_2s
-        ]
+    def calc_blade_state(self, theta_2):
+        """Calculates the geometric state of the blade for a givent theta_2 value
 
-        g_points = [self.calc_point_g(o_4_points[0], theta[1]) for theta in thetas]
+        Args:
+            theta_2 (float): In radian
 
+        Returns:
+            BladeState: Representing the geometric state of the blade
+        """
+        theta_34 = self.calc_theta_3_4(theta_2)
+        o_4 = self.calc_point_o_4()
+        a = self.calc_point_a(theta_2, o_4)
+        b = self.calc_point_b(theta_34[1], o_4)
+        g = self.calc_point_g(theta_34[1], o_4)
+        return BladeState(o_4, a, b, g)
+
+    def calc_blade_states(self, start_theta_2, end_theta_2, num_points=NUM_POINTS):
+        """Calculates blade states for a range of theta_2 values
+
+        Args:
+            start_theta_2 (float): In radians
+            end_theta_2 (float): In radians
+            num_points (int, optional): Number of points to calculate blade state for. Defaults to NUM_POINTS.
+
+        Returns:
+            (BladeState): List of blade states over the given range of theta_2 values
+        """
+        theta_2s = np.arange(
+            start_theta_2, end_theta_2, (end_theta_2 - start_theta_2) / num_points
+        )
+        return [self.calc_blade_state(theta_2) for theta_2 in theta_2s]
+
+    def plot_blade_motion(self, blade_states):
+        """Plots the motion of the blade through a series of blade states
+
+        Args:
+            blade_states ([BladeState]): Geometric states of the blade to plot
+        """
         plt.figure()
 
         x = [
             [
                 0,
-                o_4_points[i][0],
+                blade_state.o_4.x,
                 0,
-                a_points[i][0],
-                b_points[i][0],
-                o_4_points[i][0],
-                g_points[i][0],
-                b_points[i][0],
+                blade_state.a.x,
+                blade_state.b.x,
+                blade_state.o_4.x,
+                blade_state.g.x,
+                blade_state.b.x,
             ]
-            for i in range(len(thetas))
+            for blade_state in blade_states
         ]
         y = [
             [
                 0,
-                o_4_points[i][1],
+                blade_state.o_4.y,
                 0,
-                a_points[i][1],
-                b_points[i][1],
-                o_4_points[i][1],
-                g_points[i][1],
-                b_points[i][1],
+                blade_state.a.y,
+                blade_state.b.y,
+                blade_state.o_4.y,
+                blade_state.g.y,
+                blade_state.b.y,
             ]
-            for i in range(len(thetas))
+            for blade_state in blade_states
         ]
 
         fig, ax = plt.subplots()
         fig.set_size_inches(10, 10)
         (line,) = ax.plot(x[0], y[0], color="k")
         ani = animation.FuncAnimation(
-            fig, self.update, len(x), fargs=[x, y, line], interval=1, blit=False
+            fig, self.update_line, len(x), fargs=[x, y, line], interval=1, blit=False
         )
-        ani.save("test.gif")
         plt.show()
 
-    def update(self, num, x, y, line):
+    def update_line(self, num, x, y, line):
         line.set_data(x[num], y[num])
-        line.axes.axis([-100, 100, -100, 100])
+        line.axes.axis(self.PLOT_RANGE)
         return (line,)
 
-    def plot_b(self):
-        theta_2s = np.arange(200, 280, 0.1)
-        thetas = [self.calc_thetas(theta_2 * np.pi / 180) for theta_2 in theta_2s]
-        b_xs = [self.b * math.cos(theta[1]) for theta in thetas]
-        b_ys = [self.b * math.sin(theta[1]) for theta in thetas]
-        # plt.plot(theta_2s, b_xs, label="x")
-        # plt.plot(theta_2s, b_ys, label="y")
+    def calc_point_a(self, theta_2, o_4):
+        """Calculates x and y coordinates of point a
 
-        plt.plot(b_xs, b_ys, label="x")
-        plt.legend()
-        plt.show()
+        Args:
+            theta_2 (float): In radians
+            o_4 ((x:float,y:float)): in mm
 
-    def calc_point_b(self, theta_4, o2):
-        return (o2[0] + self.d * math.cos(theta_4), o2[1] + self.d * math.sin(theta_4))
-
-    def calc_point_a(self, theta_4, o4):
-        return (o4[0] + self.c * math.cos(theta_4), o4[1] + self.c * math.sin(theta_4))
-
-    def calc_point_o4(self):
-        return (self.d * math.cos(self.theta_1), self.d * math.sin(self.theta_1))
-
-    def calc_point_g(self, o4, theta_4):
-        return (
-            o4[0] + self.g * math.cos(theta_4 + self.theta_5),
-            o4[1] + self.g * math.sin(theta_4 + self.theta_5),
+        Returns:
+            (x:float, y:float): Coordinates of point a
+        """
+        return Coordinate(
+            o_4.x + self.c * math.cos(theta_2),
+            o_4.y + self.c * math.sin(theta_2),
         )
 
-    def calc_thetas(self, theta_2):
-        return fsolve(functools.partial(self.equations, theta_2=theta_2), (0, 0))
+    def calc_point_b(self, theta_4, o_2):
+        """Calculates x and y coordinates of point b
 
-    def equations(self, guess, theta_2):
+        Args:
+            theta_4 (float): In radians
+            o_2 ((x:float,y:float)): in mm
+
+        Returns:
+            (x:float, y:float): Coordinates of point b
+        """
+        return Coordinate(
+            o_2.x + self.d * math.cos(theta_4),
+            o_2.y + self.d * math.sin(theta_4),
+        )
+
+    def calc_point_g(self, theta_4, o_4):
+        """Calculates x and y coordinates of point g
+
+        Args:
+            theta_4 (float): In radians
+            o_4 ((x:float,y:float)): in mm
+
+        Returns:
+            (x:float, y:float): Coordinates of point g
+        """
+        return Coordinate(
+            o_4.x + self.g * math.cos(theta_4 + self.theta_5),
+            o_4.y + self.g * math.sin(theta_4 + self.theta_5),
+        )
+
+    def calc_point_o_4(self):
+        """Calculates x and y coordinates of point o_4
+
+        Returns:
+            (coordinate): Coordinates of point o_4
+        """
+        return Coordinate(
+            self.d * math.cos(self.theta_1), self.d * math.sin(self.theta_1)
+        )
+
+    def calc_theta_3_4(self, theta_2):
+        """Calculates values of theta 3 and theta 4 given a theta 2 value
+
+        Args:
+            theta_2 (float): In radians
+
+        Returns:
+            (theta_3: float, theta_4: float): In radians
+        """
+        return fsolve(
+            functools.partial(self.closed_loop_equations, theta_2=theta_2), (0, 0)
+        )
+
+    def closed_loop_equations(self, guess, theta_2):
         """Closed-loop equations for four bar mechanism of a blade
 
         Args:
