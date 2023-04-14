540a1231b2155df9a57bd86002d76182a479396e
Samuel Lehmann <samueljlehmann@gmail.com>
2023-04-14 08:27:20 -0700

    Outline ring

--- iris.py
+++ iris.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from blade import Blade
+from ring import Ring
 
 
 class Iris:
     PLOT_RANGE = [-100, 100, -100, 100]
 
-    def __init__(self, blade_count, a, b, c, d, e, f, g, theta_5) -> None:
+    def __init__(
+        self, blade_count, a, b, c, d, e, f, g, theta_5, start_theta_2, end_theta_2
+    ):
         self.blade_count = blade_count
         self.a = a
         self.b = b
@@ -20,7 +23,9 @@ class Iris:
         self.f = f
         self.g = g
         self.theta_5 = theta_5
-
+        self.start_theta_2 = start_theta_2
+        self.end_theta_2 = end_theta_2
+        self.ring = Ring(blade_count, end_theta_2 - start_theta_2, theta_5, a)
         self.blades = [
             Blade(
                 a,
@@ -38,13 +43,17 @@ class Iris:
             for i in range(blade_count)
         ]
 
-    def animateIris(self, start_theta_2, end_theta_2):
+    def animateIris(self, start_theta_2=None, end_theta_2=None):
         """Plots the motion of the iris through a range of theta_2 values
 
         Args:
             start_theta_2 (float): Beginning theta_2 value for all blades
             end_theta_2 (float): Ending theta_2 value for all blades
         """
+
+        if start_theta_2 is None or end_theta_2 is None:
+            start_theta_2 = self.start_theta_2
+            end_theta_2 = self.end_theta_2
 
         x_y_paths = [
             blade.get_x_y_motion_coordinates(
@@ -70,6 +79,7 @@ class Iris:
             interval=1,
             blit=False,
         )
+        self.ring.draw(ax)
         plt.show()
 
     def update_lines(self, num, x_y_paths, lines):
@@ -79,5 +89,17 @@ class Iris:
         return (line,)
 
 
-iris = Iris(16, 66.5, 42, 39.5, 36.5, 41.5, 35, 72, 60 * np.pi / 180)
-iris.animateIris(200 * np.pi / 180, 230 * np.pi / 180)
+iris = Iris(
+    3,
+    66.5,
+    42,
+    39.5,
+    36.5,
+    41.5,
+    35,
+    72,
+    60 * np.pi / 180,
+    200 * np.pi / 180,
+    230 * np.pi / 180,
+)
+iris.animateIris()
